272adf3f28dad45a4984a1c7a2f16ab0c51e8756
Aaron Zou <aaron.zou@utexas.edu>
2018-04-03 14:38:14 -0500

    set up exported include directories correctly for lib/

--- sssp-cpu-only/src/main.cpp
+++ sssp-cpu-only/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <thread>
+#include "graph.hpp"
 
 int main(int argc, const char *argv[]) {
+  auto dummy = Graph{};
   auto thread = std::thread{[]() { std::cout << "Hello world!" << std::endl; }};
   thread.join();
   return 0;
