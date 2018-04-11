de6fd627846b583b1ded785973c16b33a3bb7f91
chudooder <codeshawnachu@gmail.com>
2018-04-11 15:29:08 -0500

    sssp-opencl skeleton

---
+++ sssp-opencl/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <CL/cl2.hpp>
+
+int main(int argc, const char *argv[]) {
+    std::cout << "lmeo" << std::endl;
+    //get all platforms (drivers)
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(all_platforms.size()==0){
+        std::cout<<" No platforms found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
+}
